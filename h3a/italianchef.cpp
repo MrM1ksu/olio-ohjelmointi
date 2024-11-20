cf937c838f23fae217aeb2e1b2ab543d8836c220
MrM1ksu <miksu.mr.00@gmail.com>
2024-11-20 12:24:55 +0200

    tehtävä h3a valmis

---
+++ h3a/italianchef.cpp
@@ -0,0 +1,22 @@
+#include "italianchef.h"
+
+ItalianChef::ItalianChef(string n) : Chef(n)
+{
+    name=n;
+    cout<<"ItalianChef "<<name<<" konstruktori"<<endl;
+}
+
+ItalianChef::~ItalianChef()
+{
+    cout<<"ItalianChef "<<name<<" destruktori"<<endl;
+}
+
+string ItalianChef::getName()
+{
+    return name;
+}
+
+void ItalianChef::makePasta()
+{
+    cout<<"Italian Chef "<<name<<" makes pasta"<<endl;
+}
---
+++ h3a/main.cpp
@@ -0,0 +1,19 @@
+#include "chef.h"
+#include "italianchef.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Chef objChef("Gordon Ramsay");
+     objChef.makeSalad();
+     objChef.makeSoup();
+
+    ItalianChef objChef2("Anthony Bourdain");
+    objChef2.makeSalad();
+    objChef2.makeSoup();
+    objChef2.makePasta();
+    objChef2.getName();
+    cout<<"Name of the Italian Chef is "<<objChef2.getName()<<endl;
+    return 0;
+}
